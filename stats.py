5423ed7c8dacdee7c8f930f56224a26bfee07f0c
andrewmerluzzi <andrewmerluzzi@gmail.com>
2015-08-27 14:08:20 -0500

    did nosetests and everything worked as expected. stats and test_stats work at this point

--- stats.py
+++ stats.py
@@ -1,15 +1,6 @@
 def mean(vals):
-	total = sum(vals)
+	total = float(sum(vals))
 	length = len(vals)
 	return total/length
 
-def test_mean():
-	assert(mean([2,4]) == 3)
-test_mean()
-
-def test_float_mean():
-	assert(mean([1,2]) == 1.5)
-test_float_mean()
-
-#print mean([2,4])
-
+# print mean ([2,4])
