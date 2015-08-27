4127927bfdf1117b0f6d50f42da1efcf92f1da83
andrewmerluzzi <andrewmerluzzi@gmail.com>
2015-08-27 13:24:43 -0500

    added separate test file copied from the original stats file

--- stats.py
+++ stats.py
@@ -3,5 +3,13 @@ def mean(vals):
 	length = len(vals)
 	return total/length
 
-print mean([2,4])
+def test_mean():
+	assert(mean([2,4]) == 3)
+test_mean()
+
+def test_float_mean():
+	assert(mean([1,2]) == 1.5)
+test_float_mean()
+
+#print mean([2,4])
 
